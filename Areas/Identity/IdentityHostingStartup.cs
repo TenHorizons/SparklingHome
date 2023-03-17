680cfe2e76dd0a2177a91b133c2d735a35b0a8d8
TenHorizons <40300779+TenHorizons@users.noreply.github.com>
2023-03-17 16:13:30 +0800

    Added Register and Login Function
    Followed PDF 1.5 to add login and register function

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SparklingHome.Areas.Identity.Data;
+using SparklingHome.Data;
+
+[assembly: HostingStartup(typeof(SparklingHome.Areas.Identity.IdentityHostingStartup))]
+namespace SparklingHome.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SparklingHomeContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SparklingHomeContextConnection")));
+
+                services.AddDefaultIdentity<SparklingHomeUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SparklingHomeContext>();
+            });
+        }
+    }
+}
